5bb3a433f730c0b5eec2b77274d43fc45f426e81
Midori Takahashi <mintgreen7programming@gmail.com>
2022-10-27 16:23:07 +0900

    last

---
+++ db/migrate/20221027070231_change_column_on_title_body.rb
@@ -0,0 +1,5 @@
+class ChangeColumnOnTitleBody < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :posts, :title, :body, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_26_063930) do
+ActiveRecord::Schema.define(version: 2022_10_27_070231) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
